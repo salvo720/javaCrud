c313de26efe7ce202cf4e2641587bceb193524f3
salvo720 <salvocelano91@gmail.com>
2022-09-10 20:09:29 +0200

    update database file

--- src/main/resources/static/database/javacrud.sql
+++ src/main/resources/static/database/javacrud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Creato il: Set 01, 2022 alle 21:32
+-- Creato il: Set 10, 2022 alle 18:06
 -- Versione del server: 8.0.28
 -- Versione PHP: 7.4.28
 
@@ -42,7 +42,9 @@ INSERT INTO `elemento` (`id`, `nome`, `serialeProdotto`, `quantita`) VALUES
 (3, 'albicocche', '65473546', 1000),
 (4, 'fragole', '95145235', 200),
 (5, 'limoni', '65473546', 1000),
-(6, 'alberi', '95145235', 200);
+(6, 'alberi', '95145235', 200),
+(7, 'asd22', 'asd2', 23),
+(37, 'asd', 'asd2', 0);
 
 -- --------------------------------------------------------
 
@@ -88,7 +90,7 @@ ALTER TABLE `utente`
 -- AUTO_INCREMENT per la tabella `elemento`
 --
 ALTER TABLE `elemento`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
 -- AUTO_INCREMENT per la tabella `utente`
